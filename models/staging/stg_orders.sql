f565724dbb590a8e29555c4640428505f6f66fd6
gkdarsh65@gmail.com <gkdarsh65@gmail.com>
2024-02-01 04:34:51 +0000

    developing stg tables

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,22 @@
+select
+--from raw_orders
+o.ORDERID,
+o.ORDERDATE,
+o.SHIPDATE,
+o.SHIPMODE,
+o.ORDERSEKKINGPRICE - o.ORDERCOSTPRICE as orderprofit,
+o.ORDERCOSTPRICE,
+o.ORDERSEKKINGPRICE,
+-- from customer
+c.CUSTOMERNAME,
+c.SEGMENT,
+c.COUNTRY,
+-- from raw_product
+p.CATEGORY,
+p.PRODUCTNAME,
+p.SUBCATEGORY
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c
+on o.CUSTOMERID = c.CUSTOMERID
+left join {{ ref('raw_product') }} as p
+on o.PRODUCTID = p.PRODUCTID
